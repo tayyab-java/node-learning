ea74035f2b51df170eb800f16c92dee2095365a3
TAYYAB LIAQAT <54269485+tayyab-java@users.noreply.github.com>
2023-10-09 00:16:12 +0500

    day1

--- 1.commonjs/index.js
+++ 1.commonjs/index.js
@@ -1,5 +1,19 @@
 // module 
 const lib = require('./lib.js')
 console.log(lib.sum(4,5),lib.diff(4,5))
+const t1 =performance.now()
 // now calling variable functions
 console.log(lib.sum1(4,5),lib.diff1(4,5))
+
+const fs= require('fs');
+const txt =fs.readFileSync('demo.txt','utf-8')
+console.log(txt);
+const t2_for_sync =performance.now()
+
+// asyncronous fileread with callback
+fs.readFile('demo.txt','utf-8',(err, txt2)=>{
+    console.log(txt2)
+})
+const t3_for_async =performance.now()
+console.log(t1,t2_for_sync,t3_for_async)
+
--- 2esmodulejs/index.js
+++ 2esmodulejs/index.js
@@ -2,3 +2,4 @@
 // ES module syntax
 import {sum2,diff2} from './esmodules.js';
 console.log(sum2(4,5),diff2(4,5))
+
